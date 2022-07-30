c6dbe0245530701675558d926d2296bd116e7008
Aayush Mishra <95838151+iamAayushMishra@users.noreply.github.com>
2022-07-30 14:07:06 +0530

    working on txt file
    working with txt files in python

--- prac-10.py
+++ prac-10.py
@@ -1,5 +1,15 @@
+#working with txt files in python
+
+
+#opening a text file in append mode
 f = open("someFile.txt", 'a+')
-print(f.closed)
+
+#printing wheater the file obj is closed or not
+print(f.closed)  #returns -> False as File is not closed
+
+#closing the file 
 f.close()
-print(f.closed)
+
+#again printing is file is closed
+print(f.closed) #returns true
 
