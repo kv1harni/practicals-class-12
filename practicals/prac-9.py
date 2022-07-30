139a3abfefb20f18a004846d0103aacfd0e79285
Aayush Mishra <95838151+iamAayushMishra@users.noreply.github.com>
2022-07-30 14:17:01 +0530

    program to read lines of  text file seperated by #

--- practicals/prac-9.py
+++ practicals/prac-9.py
@@ -1,13 +1,22 @@
 #write a program to read a text file
 
+#opening the file object using the open() method
 file = open('someFile.txt', 'r')
+
+#reading lines
 l1 = file.readlines()
+
+#empty variable to store line
 line = " "
+
+#iterating through the number of lines
 for i in range(len(l1)):
     l = l1[i].split()
     for j in l:
         line = line + j
         line = line + '#'
-
+#printing the line
 print(line)
+
+#closing the file
 file.close()
