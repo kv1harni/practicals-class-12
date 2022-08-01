a60bba60325c4585f5e7472d9569a59dac627d1d
KV Harni <109907524+kv1harni@users.noreply.github.com>
2022-08-01 12:25:17 +0530

    Binary search using python3

--- prac-7.py
+++ prac-7.py
@@ -1,20 +1,25 @@
 def binary_search(sequence, item, Lb, Ub):
+    #Perform Binary search
     if Lb > Ub :
         return -5
-    
+    #finding mid term for recurrion purpose
     mid = int((Lb + Ub)/2)
     if item == sequence[mid]:
         return mid
     elif item < sequence[mid]:
         Ub = mid - 1
         return binary_search(sequence, item, Ub, Lb)
-
+    #recussing the function
     else:
         Lb = mid + 1
         return binary_search(sequence, Lb, Ub, item)
 
+    
+#putting values in eval function
 L = eval(input("enter the elemnt in shorted order: "))
+#cheking length of input
 n = len(L)
 element = int(input("Enter the element that you want to search: "))
+#calling the function and storing as variable
 found = binary_search(L, element, 0, n-1)
 print(f"element found at index {found}")
