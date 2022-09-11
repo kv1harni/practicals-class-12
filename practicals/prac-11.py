a7b80bf534ebbfea65ebca185c8383ece9146679
milind899 <76685357+milind899@users.noreply.github.com>
2022-09-11 19:57:02 +0530

    Update prac-11.py

--- practicals/prac-11.py
+++ practicals/prac-11.py
@@ -1,10 +1,7 @@
-#Write a program to count number of words in a file
-
-file = open('story.txt', 'r')       #initiating file object 
-
-text = file.read()                  #reading the file content
-text_list = text.split()            #splitting the text by empty space and adding each word to a list
-
-print(len(text_list))               #printing out the length of the list which depicts the number count of words
-
-file.close()
+fin=open("someFile.txt",'r')
+str=fin.read( )
+L=str.split()
+count_words=0
+for i in L:   
+    count_words=count_words+1
+print(count_words)
