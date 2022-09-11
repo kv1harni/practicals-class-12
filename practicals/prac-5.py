f60493d2813638f398a682bf8377a2cd2a5b4e2d
milind899 <76685357+milind899@users.noreply.github.com>
2022-09-11 20:07:29 +0530

    Update prac-5.py

--- practicals/prac-5.py
+++ practicals/prac-5.py
@@ -1,5 +1,11 @@
-lst = [19, 29, 9 , 49 , 59, 69]
+def Sum_ListEle(LS,n):    
+    if n==0:        
+        return 0    
+    else:        
+        return LS[n-1]+Sum_ListEle(LS,n-1)
+        
+L=eval(input("enter a list of numbers : "))
+size=len(L)
+total=Sum_ListEle(L,size)
+print("Sum of list elements is :", total)
 
-sumLst = sum(lst)
-
-print(f"Sum of the list is: {sumLst}")
